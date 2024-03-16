85d73d506e997d5b3ab6dcbeb9bcfef070125d74
George Dobbs <george.dobbs@gmail.com>
2024-03-16 12:53:19 -0400

    use existing instead of redownloading cover

--- biblio.py
+++ biblio.py
@@ -25,30 +25,33 @@ def get_biblio_covers(isbns,file_base='data/covers'):
   isbn_url='https://www.biblio.com'
   session=prep_session(isbn_url,headers=headers)
   for isbn in isbns:
-    if isbn:
-      isbn=strip_xl_friendly(isbn)
-      r=session.get(isbn_url+'/'+isbn,headers=headers)
-      assert r.status_code==200,'Did not get a 200 back'
-      soup=BeautifulSoup(r.content,'lxml')
-      ts=soup.find(id='top-section')
-      if ts is None:
-        print(isbn_url+' giving alternate page not programmed')
-        filenames+=[ None]
-        continue
-      img_url=ts.find('img').attrs['src']
-      r=requests.get(img_url)
-      if r.status_code!=200:
-        print('Did not get a 200 back')
-        filenames+=[ None]
-        continue
-      filename=isbn+'.jpg'
-      filepath=os.path.sep.join([file_base,filename])
-      with open(filepath,'wb') as f:
-        f.write(r.content)
-      print('wrote cover image to: '+filepath)
-      filenames+=[filename]
+    isbn=strip_xl_friendly(isbn)
+    filename=isbn+'.jpg'
+    filepath=os.path.sep.join([file_base,filename])
+    filenames+=[filename]
+    if os.path.exists(filepath):
+      print('using existing cover image: '+filepath)
     else:
-      filenames+=[None]
+      if isbn:
+        r=session.get(isbn_url+'/'+isbn,headers=headers)
+        assert r.status_code==200,'Did not get a 200 back'
+        soup=BeautifulSoup(r.content,'lxml')
+        ts=soup.find(id='top-section')
+        if ts is None:
+          print(isbn_url+' giving alternate page not programmed')
+          filenames+=[ None]
+          continue
+        img_url=ts.find('img').attrs['src']
+        r=requests.get(img_url)
+        if r.status_code!=200:
+          print('Did not get a 200 back')
+          filenames+=[ None]
+          continue
+        with open(filepath,'wb') as f:
+          f.write(r.content)
+        print('wrote cover image to: '+filepath)
+      else:
+        filenames[-1]=None
   return filenames
 
 def enhance_covers(df):
