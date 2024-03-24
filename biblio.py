7e12911590bfb18002018d83ef463aa77da439b2
George Dobbs <george.dobbs@gmail.com>
2024-03-24 19:28:09 -0400

    logging

--- biblio.py
+++ biblio.py
@@ -1,6 +1,7 @@
 """Items to do with biblio
 """
 import os
+import logging
 import re
 
 from bs4 import BeautifulSoup
@@ -8,6 +9,8 @@ import requests
 
 from navigate import prep_session
 from parse import strip_xl_friendly
+logging.basicConfig(level=logging.INFO)
+logger=logging.getLogger(__name__)
 
 def get_biblio_covers(isbns,file_base='data/covers'):
   '''Downloads cover for isbns and returns List of True if success, else false
@@ -17,6 +20,9 @@ def get_biblio_covers(isbns,file_base='data/covers'):
   '''
   if not isinstance(isbns,list):
     isbns=[isbns]
+  
+  logger.info(f"Calling {__name__} for info on {len(isbns)} books")
+  
   filenames=[]
   headers={
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -30,26 +36,33 @@ def get_biblio_covers(isbns,file_base='data/covers'):
     filepath=os.path.sep.join([file_base,filename])
     filenames+=[filename] # provisionally this will be the name (unless an error happens)
     if os.path.exists(filepath):
-      print('using existing cover image: '+filepath)
+      logger.info('using existing cover image: '+filepath)
     else:
       if isbn:
         r=session.get(isbn_url+'/'+isbn,headers=headers)
-        assert r.status_code==200,'Did not get a 200 back'
+        if r.status_code!=200:
+          logger.warning(f'Got status {r.status_code} while loading page for {isbn}')
+          filenames[-1]=None
+          continue                         
         soup=BeautifulSoup(r.content,'lxml')
         ts=soup.find(id='top-section')
         if ts is None:
-          print(isbn_url+' giving alternate page not programmed')
+          logger.warning(f'biblio gave alternate page for {isbn}. not programmed') # e.g. https://www.biblio.com/9780593490594  (silverview)
           filenames[-1]=None
           continue
         img_url=ts.find('img').attrs['src']
-        r=requests.get(img_url)
-        if r.status_code!=200:
-          print('Did not get a 200 back')
+        if len(img_url)==0:
+          logger.warning(f'No image found for {isbn}')
           filenames[-1]=None
           continue
+        r=requests.get(img_url)
+        if r.status_code!=200:
+          logger.warning(f'Got status {r.status_code} while trying to get cover image for {isbn}')
+          filenames[-1]=None
+          continue                         
         with open(filepath,'wb') as f:
           f.write(r.content)
-        print('wrote cover image to: '+filepath)
+        logger.info('wrote cover image to: '+filepath)
       else:
         filenames[-1]=None
   return filenames
